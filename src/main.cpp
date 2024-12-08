18c4805587454c216758eed90828a00087d6fae1
Orbán Levente <orban.levente.laszlo@gmail.com>
2024-12-08 23:19:23 +0100

    Added CUPS dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
+#include <cups/cups.h>
+#include <stdio.h>
+
 #include <fstream>
-#include <nlohmann/json.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
-int main() {
+// For CUPS
+int print_dest(void *user_data, unsigned flags, cups_dest_t *dest) {
+    if (dest->instance)
+        printf("%s/%s\n", dest->name, dest->instance);
+    else
+        puts(dest->name);
+
+    return (1);
+}
+
+int main(void) {
+    // JSON
     std::ifstream f("./presets/sample.json");
     json data = json::parse(f);
-    std::cout << data.dump(4) << std::endl;
+    std::cout << "JSON:" << std::endl
+              << data.dump(4) << std::endl
+              << std::endl;
+
+    // CUPS: lists all available printers
+    std::cout << "CUPS:" << std::endl
+              << "Printers in area: " << cupsEnumDests(CUPS_DEST_FLAGS_NONE, 1000, NULL, 0, 0, print_dest, NULL) << std::endl
+              << std::endl;
 }
