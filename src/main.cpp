9fdf68a3ac63e9f2c48ab1d80fd4bf353717e2f9
Kovács <balintkovacs11@gmail.com>
2024-12-09 02:14:45 +0100

    git commit -m "some changes"

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,18 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 using json = nlohmann::json;
 
-// For CUPS
-int print_dest(void *user_data, unsigned flags, cups_dest_t *dest) {
-    if (dest->instance)
-        printf("%s/%s\n", dest->name, dest->instance);
-    else
-        puts(dest->name);
-
-    return (1);
-}
-
 int main(void) {
-    // JSON
-    std::ifstream f("./presets/sample.json");
-    json data = json::parse(f);
-    std::cout << "JSON:" << std::endl
-              << data.dump(4) << std::endl
-              << std::endl;
-
-    // CUPS: lists all available printers
-    std::cout << "CUPS:" << std::endl
-              << "Printers in area: " << cupsEnumDests(CUPS_DEST_FLAGS_NONE, 1000, NULL, 0, 0, print_dest, NULL) << std::endl
-              << std::endl;
+    // read an image
+    cv::Mat image = cv::imread("./assets/3.png", 1);
+    // create image window named "My Image"
+    cv::namedWindow("My Image");
+    // show the image on window
+    cv::imshow("My Image", image);
+    // aboszolút filmszínház
+    cv::waitKey(0);
 }
