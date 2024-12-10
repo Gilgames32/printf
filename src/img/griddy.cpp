3d2f5a0614b271ce38351f9a4ce3640323050e6e
levy <orban.levente.laszlo@gmail.com>
2024-12-10 12:01:00 +0100

    Initial tiling_generator class stump

--- src/img/griddy.cpp
+++ src/img/griddy.cpp
@@ -1,16 +1,27 @@
-/*
-generateGrid(
-    array of images
-    preset struct
-        document width
-        gutter width
-        quantity of cells
-        whether correct quanity
-)
-{
+#include <opencv2/core/core.hpp>
+#include <vector>
 
-}
+struct preset {
+    size_t document_width;
+    size_t gutter_width;
+    size_t tile_count;
+    bool fill;
+};
 
+class tiling_generator {
+    std::vector<cv::Mat> imgs;
+    preset user_preset;
 
+   public:
+    tiling_generator(const std::vector<cv::Mat>& images, const preset& input_preset) {
+        imgs = images;
+        user_preset = input_preset;
+    }
 
-*/
+    /**
+     * @brief Generates the tiling using TODO: some algorithm.
+     *
+     * @return cv::Mat The image data of the tiling.
+     */
+    cv::Mat static generate() {}
+};
