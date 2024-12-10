41014fd6d81652a4bb54e13f459980f4e0ad9946
Kovács <balintkovacs11@gmail.com>
2024-12-10 16:21:22 +0100

    mask filter

--- src/img/filters/size.cpp
+++ src/img/filters/size.cpp
@@ -1,13 +1,14 @@
 #include "size.hpp"
 
-SizeFilter::SizeFilter(int width, int height) : width(width), height(height), isUniform(true) {}
+SizeFilter::SizeFilter(int width, int height) : width(width), height(height) {}
 
-cv::Mat &SizeFilter::apply(const cv::Mat &image)
-{
-    cv::Mat resized;
+cv::Mat SizeFilter::apply(const cv::Mat &image) { return SizeFilter::resize(image, width, height); }
 
+cv::Mat SizeFilter::resize(const cv::Mat &image, int width, int height, int interDown, int interUp) {
     bool downScaling = width < image.cols || height < image.rows;
 
-    cv::resize(image, resized, cv::Size(width, height), 0, 0, downScaling ? downScalingInterpolation : upScalingInterpolation);
+    cv::Mat resized;
+    cv::resize(image, resized, cv::Size(width, height), 0, 0, downScaling ? interDown : interUp);
+
     return resized;
 }
