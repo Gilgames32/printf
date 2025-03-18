4e31d2f16aa660e38a6570c7498a533be667160f
Kovács <balintkovacs11@gmail.com>
2025-03-18 17:47:47 +0100

    ui building setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,11 @@
-#include <cups/cups.h>
-#include <stdio.h>
+#include "mainwindow.hpp"
 
-#include <fstream>
-#include <iostream>
-#include <nlohmann/json.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <QApplication>
 
-#include "image_source.hpp"
-#include "rotate.hpp"
-#include "size.hpp"
-#include "document_preset.hpp"
-#include "grid_tiling.hpp"
-#include "padding.hpp"
-
-int main(void) {
-    DocumentPreset preset = DocumentPreset("./presets/document/tekercs.json");
-    
-    std::cout << preset.get_document_width_px() << std::endl;
-
-
-    cv::Mat source = cv::imread("./assets/3.png");
-
-    ImageSource img = ImageSource(source, 11);
-
-    img.add_filter(new PaddingFilter(100, true));
-
-    // img.add_filter(new RotateFilter());
-
-    GridTiling tiling = GridTiling();
-    cv::Mat result = tiling.generate(preset, {&img});
-
-
-    cv::namedWindow("My Image", cv::WINDOW_AUTOSIZE | cv::WINDOW_GUI_NORMAL);
-    cv::imshow("My Image", SizeFilter::resize_to_width(result, 1080));
-    // cv::imshow("My Image", result);
-    cv::waitKey(0);
-
-
-    return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
