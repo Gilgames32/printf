856af3afea9ef4e6269ee284688e7bfbcad7b3ca
Kovács <balintkovacs11@gmail.com>
2025-05-03 21:26:36 +0200

    fix a memory leak

--- src/ui/generator_view.cpp
+++ src/ui/generator_view.cpp
@@ -1,29 +1,36 @@
 #include "generator_view.hpp"
 
 #include <iostream>
+
 #include "grid_tiling.hpp"
 #include "preview_provider.hpp"
 
 GeneratorView::GeneratorView() {}
 
 void GeneratorView::generate(const DocumentPreset& properties, QList<ImageSource*> sources) {
+    // TODO cleanup
     qDebug() << "Generating...";
     qDebug() << "Roll Width:" << properties.get_document_width_px();
     qDebug() << "Count of sources:" << sources.size();
 
     std::vector<ImageSource*> sources_vector(sources.constBegin(), sources.constEnd());
-    GridTiling tiling = GridTiling(); // TODO: other tiling methods
+    GridTiling tiling = GridTiling();  // TODO: other tiling methods
     cv::Mat result = tiling.generate(properties, sources_vector);
 
     if (result.empty()) {
         qDebug() << "Failed to generate image.";
+        for (auto img : sources) {
+            delete img;
+        }
         return;
     }
 
     // Convert cv::Mat to QImage
     QImage image(result.data, result.cols, result.rows, result.step, QImage::Format_RGB888);
     PreviewProvider::instance()->setImage(image.rgbSwapped());
+    for (auto img : sources) {
+        delete img;
+    }
 
     qDebug() << "Image generation completed and set to provider.";
 }
-
